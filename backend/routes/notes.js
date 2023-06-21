7d97edac26a1dc7d023791270291fefdf2cb0f70
SravanKosaraju <sravankosaraju123@gmail.com>
2023-06-21 13:27:15 +0530

    Adding Update Notes Endpoint

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -30,4 +30,26 @@ router.post('/addnotes', fetchuser, [
     }
 })
 
+router.put('/updatenotes/:id', fetchuser, async (req, res) => {
+    const {title,description,tag}=req.body
+    const newNote={};
+    if(title){newNote.title=title}
+    if(description){newNote.description=description}
+    if(tag){newNote.tag=tag}
+    let note=await Notes.findById(req.params.id)
+    if(!note){
+        return res.status(404).send("Not Found")
+    }
+    if(note.user.toString()!==req.user.id){
+        return res.status(401).send("Not Allowed")
+    }try {
+        note=await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+        res.json({note})
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send("External Server Error")
+    }
+    
+}) 
+
 module.exports = router  
