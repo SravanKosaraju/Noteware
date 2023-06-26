9de94b673c8aaeac7d24f8b9f35beb9764d7d3d6
SravanKosaraju <sravankosaraju123@gmail.com>
2023-06-26 13:05:54 +0530

    Displaying Notes Using UseContext Hook

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import { useContext } from 'react'
+import Notecontext from '../context/notes/NoteContext'
+
+const About = () => {
+    
+    return (
+        <div>
+            This is about 
+        </div>
+    )
+}
+
+export default About
+
